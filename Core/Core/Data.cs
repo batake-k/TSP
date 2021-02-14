41e8987cc96877a4135215e4bc8982e26304eaac
batake_k <takabatake621@gmail.co.jp>
2021-02-14 22:45:40 +0900

    #1 雛型の作成

---
+++ Core/Core/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class Data
+    {
+        public Data(in string input_file)
+        {
+            num_cities = 4;
+            distances = new float[4, 4] {
+                { 0.00f, 1.00f, 1.41f, 1.00f},
+                { 1.00f, 0.00f, 1.00f, 1.41f},
+                { 1.41f, 1.00f, 0.00f, 1.00f},
+                { 1.00f, 1.41f, 1.00f, 0.00f},
+            };
+        }
+
+        public float getDistance(int i, int j)
+        {
+            return distances[i,j];
+        }
+
+        public int num_cities {set; get;}
+        private float[,] distances;
+    }
+}
---
+++ Core/Core/Solver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    class Solver
+    {
+        public Solver(in Data _data)
+        {
+            data = _data;
+
+            calculated_best_distance = float.MaxValue;
+            calculated_best_route = new int[0];
+        }
+
+        public virtual bool run()
+        {
+            return false;
+        }
+
+        protected float calculateRouteDistance(in int[] route)
+        {
+            float distance = 0;
+            for(int i = 0; i < route.Length - 1; i++)
+            {
+                distance += data.getDistance(route[i], route[i + 1]);
+            }
+            distance += data.getDistance(route[route.Length - 1], 0);
+
+            return distance;
+        }
+
+        protected Data data;
+
+        public float calculated_best_distance { set; get; }
+        public int[] calculated_best_route { set; get; }
+    }
+}
